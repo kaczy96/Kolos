d809c0f6477e2b7614b1aba976a116afa1466416
Tomasz Niedźwiedź <kaczystudio@gmail.com>
2018-01-25 12:48:11 +0100

    Add files via upload

---
+++ PozycjaZamowienia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolos
+{
+    class PozycjaZamowienia
+    {
+        public string nazwaTowaru { get; set; }
+        public float cenaJednostkowa { get; set; }
+        public float stawkaVAT = 0.22f;
+        public float ilosc { get; set; }
+
+        public double obliczBrutto()
+        {
+            double cenaBrutto = cenaJednostkowa * ilosc;
+            return cenaBrutto;
+        }
+
+        public double obliczNetto()
+        {
+            double cenaNetto = cenaJednostkowa * ilosc - ((cenaJednostkowa * ilosc) * stawkaVAT);
+            return cenaNetto;
+        }
+    }
+}
---
+++ Zamowienie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolos
+{
+    class Zamowienie
+    {
+        public DateTime dataRealizacji { get; set; }
+        public bool statusZamowienia { get; set; }
+        public string identyfikatorZamowienia { get; set; }
+        public bool potwierdzenieElektroniczne { get; set; }
+
+        public void wartoscZamowienia()
+        {
+            Console.WriteLine("Cena Brutto: {0}", PozycjaZamowienia.);
+        }
+
+    }
+}
